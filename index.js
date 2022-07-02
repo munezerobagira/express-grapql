b299dfb7afaf7aca3726a141dbd1632321028812
Sostene MUNEZERO BAGIRA <munezerobagira@gmail.com>
2022-07-02 23:37:24 +0200

    simple Object relations

--- index.js
+++ index.js
@@ -1,31 +1,65 @@
 require("dotenv/config");
 const express = require("express");
-const { buildSchema } = require("graphql");
+const {
+  buildSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLSchema,
+} = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
 const { PORT = 5000 } = process.env;
 const tasks = require("./tasks.json");
-const schema = buildSchema(
-  ` type Query{
-        hello:[Task]
-    }
-    type Task{
-        id: ID,
-        name: String,
-        deadline: String,
-    }
-    `
-);
-var root = {
-  hello: () => {
-    return tasks;
+const projects = require("./projects.json");
+const TaskType = new GraphQLObjectType({
+  name: "Task",
+  fields: () => ({
+    id: { type: GraphQLID },
+    description: { type: GraphQLString },
+    name: { type: GraphQLString },
+    deadline: { type: GraphQLString },
+    project: {
+      type: ProjectType,
+      resolve(parent, args) {
+        return projects.find((project) => project.id == parent.projectId);
+      },
+    },
+  }),
+});
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }),
+});
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    task: {
+      type: TaskType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return tasks.find((task) => task.id == args.id);
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
   },
-};
+});
+const schema = new GraphQLSchema({
+  query: RootQuery,
+});
 const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
     schema,
-    rootValue: root,
     graphiql: true,
   })
 );
